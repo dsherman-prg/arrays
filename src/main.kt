747610563a15cce7dd1897c3ed7c1c424bbacdaa
DragonOfDissent <dvpsherman@gmail.com>
2021-09-27 16:56:05 -0500

    Adding comments

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,8 @@
 fun main(){
-    var a = arrayOf("Hotdog","Hotdog Buns","Ketchup","Celery Salt","Mustard")
-    for (entry in a){
+    // Ingredients for hotdog stand
+    var hotdogStand = arrayOf("Hotdog","Hotdog Buns","Ketchup","Celery Salt","Mustard")
+    // Print everything in the hotdogStand array
+    for (entry in hotdogStand){
         println(entry)
     }
 }
